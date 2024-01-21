bb888a606089b6b373dc23b6d81f5eaf2b9a0a58
Corbelli Mattia <m.corbelli098@gmail.com>
2024-01-21 14:28:07 +0100

    fixed logo spacing in footer

--- src/app/(landing)/_components/footer.tsx
+++ src/app/(landing)/_components/footer.tsx
@@ -26,7 +26,7 @@ function Footer() {
   return (
     <footer className="w-full max-w-screen-2xl mx-auto p-4 md:py-8">
       <div className="sm:flex sm:items-center sm:justify-between">
-        <LogoPortfolio className="flex items-center mb-4 sm:mb-0 space-x-3" />
+        <LogoPortfolio className="flex items-center mb-4 sm:mb-0" />
         <NavigationMenu className="flex" navElements={navElements} />
       </div>
       <Separator className="my-6 bg-gray-300 sm:mx-auto lg:my-8" />
