61819cb8a479a19b66d1c04a7ee47f298aa2b558
Corbelli Mattia <m.corbelli098@gmail.com>
2024-01-22 14:18:27 +0100

    added not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 function NotFoundPage() {
   return (
-    <main className="h-screen flex justify-center items-center">
-      NOT-FOUND PAGE
+    <main className="flex items-center justify-center h-screen flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <h1 className="text-3xl md:text-5xl text-center text-neutral-800m mb-4">
+          Page not found
+        </h1>
+        <div className="text-sm md:text-xl text-neutral-400 max-w-xs md:max-w-2xl text-center mx-auto">
+          Could not find requested resource
+        </div>
+        <Button className="mt-8" size="lg" asChild>
+          <Link href="/">Return to HomePage</Link>
+        </Button>
+      </div>
     </main>
   );
 }
